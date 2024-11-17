4a51afa250de84e7b5794e21c1d7b1c5f2241982
Darmendren Ravindran <darmendrenravindran@gmail.com>
2024-11-18 00:29:35 +0800

    logging and Exception

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ HYPEN_E_DOT='-e .'
 def get_requirements(file_path:str)->List[str]:
     '''
     this function will return list of requirments
-    
     '''
     requirements =[]
     with open(file_path) as file_obj:
