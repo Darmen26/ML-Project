77179c0f7cece7bf1b95516b5b1f09bba42d321d
Darmendren Ravindran <darmendrenravindran@gmail.com>
2024-11-22 23:23:13 +0800

    Model Training and Model Evaluation Component

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,12 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+
 @dataclass
 class DataIngestionConfig:
     train_data_path:str=os.path.join('artifact','train.csv')
@@ -26,6 +32,8 @@ class DataIngestion:
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
             
             df.to_csv(self.ingestion_config.raw_data_path,index =False,header= True)
+            logging.info('Raw dataset saved succesfully')
+            
             
             logging.info("Train test split initiated")
             train_set,test_set= train_test_split(df,test_size=0.2,random_state= 42)
@@ -46,5 +54,11 @@ class DataIngestion:
             raise CustomException(e,sys)
 if __name__ == '__main__':
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data = obj.initiate_data_ingestion()
+    
+    data_tranformation = DataTransformation()
+    train_arr,test_arr,_= data_tranformation.initiate_data_transformation(train_data,test_data)
+    
+    modeltrainer= ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
         
